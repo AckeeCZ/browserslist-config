8605a1263df5e68034dc9a4ce202e733142f0734
Jiří Čermák <jiri.cermak@ackee.cz>
2019-11-29 21:01:28 +0100

    :tada: Create a custom shared browserslist

---
+++ index.js
@@ -0,0 +1,12 @@
+// See full list of available browsers at https://github.com/browserslist/browserslist#browsers
+module.exports = [
+  "Chrome",
+  "Firefox",
+  "iOS",
+  "Opera",
+  "Safari",
+  "Samsung",
+  "UCAndroid",
+  "Edge",
+  "ChromeAndroid"
+].map(browser => `last 2 ${browser} versions`);
