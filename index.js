a4812131e668d00cb367a7df8abc4ca4ce03bf6d
Jiří Čermák <jiri.cermak@ackee.cz>
2023-02-17 13:36:04 +0100

    Update production query
    https://browsersl.ist/#q=%3E0.2%25%2C+not+dead%2C+not+ie+%3C%3D+11%2C+not+op_mini+all%2C+not+safari+%3C%3D+14%2C+not+ios+%3C%3D+14+and+supports+flexbox-gap

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ module.exports = {
 		'last 1 safari version',
 	],
 	production: [
-		'>0.2%',
-		'not dead',
-		'not ie <= 11',
-		'not op_mini all',
-		'not safari <= 8',
+		'>0.2%'
+		'not dead'
+		'not ie <= 11'
+		'not op_mini all'
+		'not safari <= 14'
+		'not ios <= 14 and supports flexbox-gap'
 	],
 };
