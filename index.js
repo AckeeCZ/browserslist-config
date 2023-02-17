260315d9b440b670360dc8fdfba6f1e88db11375
Jiří Čermák <jiri.cermak@ackee.cz>
2023-02-17 13:39:26 +0100

    🔧 Update browserslist

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ module.exports = {
 		'last 1 safari version',
 	],
 	production: [
-		'>0.2%'
-		'not dead'
-		'not ie <= 11'
-		'not op_mini all'
-		'not safari <= 14'
-		'not ios <= 14 and supports flexbox-gap'
+		'>0.2%',
+		'not dead',
+		'not ie <= 11',
+		'not op_mini all',
+		'not safari <= 14',
+		'not ios <= 14 and supports flexbox-gap',
 	],
 };
